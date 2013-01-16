52903c4ae62a5d95c02ec36bccf5905829f83e9c
Your Name <tamu222i@www5042u.sakura.ne.jp>
2013-01-16 20:35:22 +0900

    1.2

---
+++ alphanumeric.rb
@@ -0,0 +1,3 @@
+def to_alphanumeric(s)
+  s.gsub /[^\w\s]/, ''
+end
---
+++ test_alphanumeric.rb
@@ -0,0 +1,7 @@
+require 'test/unit'
+require 'alphanumeric'
+class ToAlphanumericTest < Test::Unit::TestCase
+  def test_strips_non_alphanumeric_characters
+    assert_equal '3 the Magic Number', to_alphanumeric('#3, the *Magic, Number*?')
+  end
+end
