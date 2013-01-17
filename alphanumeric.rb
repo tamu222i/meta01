8a3e2e4fb3ef21bd96194d2b5335217cbcabdeac
Your Name <tamu222i@www5042u.sakura.ne.jp>
2013-01-17 21:25:09 +0900

    open class

--- alphanumeric.rb
+++ alphanumeric.rb
@@ -1,13 +1,25 @@
 
-if $0 == __FILE__
-require 'test/unit'
-class ToAlphanumericTest < Test::Unit::TestCase
-  def test_strips_non_alphanumeric_characters
-    assert_equal '3 the Magic Number', to_alphanumeric('#3, the *Magic, Number*?')
-  end
-end
-end
-
 def to_alphanumeric(s)
   s.gsub /[^\w\s]/, ''
 end
+
+class String
+  def to_alphanumeric
+    gsub /[^\w\s]/, ''
+  end
+end
+
+if $0 == __FILE__
+  require 'test/unit'
+  class ToAlphanumericTest < Test::Unit::TestCase
+    def test_strips_non_alphanumeric_characters
+      assert_equal '3 the Magic Number', to_alphanumeric('#3, the *Magic, Number*?')
+    end
+  end
+  class StringExtensionsTest < Test::Unit::TestCase
+    def test_strips_non_alphanumeric_characters
+      assert_equal '3 the Magic Number', '#3, the *Magic, Number*?'.to_alphanumeric
+    end
+  end
+end
+
